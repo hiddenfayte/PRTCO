7bbea4719b8cff9e3eb09b6ce1cdf19ed455b478
hiddenfayte <58782183+hiddenfayte@users.noreply.github.com>
2023-05-23 16:43:27 -0600

    Add files via upload

--- TCOCleaner.ps1
+++ TCOCleaner.ps1
@@ -3,15 +3,13 @@
 $fileLoca1 = $Env:userprofile + '\Downloads'
 $fileLoca2 = $Env:userprofile + '\Pictures'
 $fileLoca3 = $Env:userprofile + '\Music'
-
-<#
 $fileLoca4 = $Env:userprofile + '\Desktop'
 
-#>
 
-ROBOCOPY $fileLoca1 C:\tempToDelete /E /MOVE /minage:3  /XF *.url *.lnk *.rdp 
+ROBOCOPY $fileLoca1 C:\tempToDelete /E /MOVE /minage:3  /XF *.url *.lnk *.rdp
 ROBOCOPY $fileLoca2 C:\tempToDelete /E /MOVE /minage:3  /XF *.url *.lnk *.rdp
 ROBOCOPY $fileLoca3 C:\tempToDelete /E /MOVE /minage:3  /XF *.url *.lnk *.rdp
+ROBOCOPY $fileLoca4 C:\tempToDelete /E /MOVE /minage:3  /XF *.url *.lnk *.rdp
 
 If(!(test-path -PathType container $fileLoca1))
 {
@@ -25,10 +23,10 @@ If(!(test-path -PathType container $fileLoca3))
 {
       New-Item -ItemType Directory -Path $fileLoca3
 }
- 
-<#
-ROBOCOPY $fileLoca4 C:\tempToDelete /E /MOVE /minage:3  /XF *.url *.lnk *.rdp
-#>
+If(!(test-path -PathType container $fileLoca4))
+{
+      New-Item -ItemType Directory -Path $fileLoca4
+}
 
 Add-Type -AssemblyName Microsoft.VisualBasic
 [Microsoft.VisualBasic.FileIO.FileSystem]::Deletedirectory('C:\tempToDelete','OnlyErrorDialogs','SendToRecycleBin')
